12b63e50ab5cbdf63464fb84f6960a7d7080a9d8
GibsonSWE <jacob.henriksen94@gmail.com>
2023-04-03 15:42:52 +0200

    Tweaked some tabs and spacings

--- loganalyzer.py
+++ loganalyzer.py
@@ -48,10 +48,10 @@ def notice(fileName):
 		return
 
 def availArgs():
-	print('\nAvaliable arguments:')
+	print('Avaliable arguments:')
 	print('\tstatistics\t- Show statistics for logfile')
-	print('\terror\t- Show a summary of error log entries')
-	print('\tnotice\t- Show a summary of notice log entries')
+	print('\terror\t\t- Show a summary of error log entries')
+	print('\tnotice\t\t- Show a summary of notice log entries')
 
 
 
@@ -74,28 +74,7 @@ if __name__ == "__main__":
 			elif argument == 'notice':
 				notice(fileName)
 			else:
-				print('Invalid argument.')
+				print('\nInvalid argument.')
 				availArgs()
 		else:
-			print('Invalid input.')
-
-
-#The script is used as follows:
-#python loganalyzer.py filepath actionwhere
-#filepath is the path to the log file to analyze and action is the action required by the script.
-#Valid values for the action are statistics, error, notice. 
-#The action determines what the script outputs.
-
-#statistics – Prints the statistics as follows:
-#errors 340 
-#notice 450 
-#where the first value is the type of log entry and the second value is the number of such entries there are in the file.
-
-#error – Prints the errors inthe file with each row as follows:
-#date message
-
-#notice - Prints the notice in the file with each row as follows:
-#date message
-
-#The script reads a logfile in the format seen in the provided sample file test.log.
-#Use that file to evaluate your script.
+			print('\nInvalid input.')
